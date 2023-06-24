520502fcc1eafe2ca81ef40879129b01e6e5d5bd
David Nagy <mrnagydavid@users.noreply.github.com>
2023-06-24 17:28:37 +0200

    chore: add tesla (#5)

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule ExSzamlazzHu.MixProject do
 
   defp deps do
     [
+      {:tesla, "~> 1.7.0"},
       {:ex_doc, "~> 0.29.4", only: :dev},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
