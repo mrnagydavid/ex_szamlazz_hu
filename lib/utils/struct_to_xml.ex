4fd93d9e5f48ed5387bbe7a92862771744fe7a01
mrnagydavid <mrnagydavid@gmail.com>
2023-11-15 18:34:55 +0100

    chore: refactor converter logic

--- lib/utils/struct_to_xml.ex
+++ lib/utils/struct_to_xml.ex
@@ -11,28 +11,9 @@ defmodule ExSzamlazzHu.Utils.StructToXML do
   def convert(module, tag, attributes, content, top_level \\ true) do
     content =
       content
-      |> Enum.map(fn
-        tag when is_atom(tag) -> {tag, nil, Map.get(module, tag)}
-        other -> other
-      end)
-      |> Enum.reject(fn
-        {_, _, value} -> is_nil(value)
-        _ -> false
-      end)
-      |> Enum.map(fn
-        struct when is_struct(struct) ->
-          convert(struct, false)
-
-        {_, _, struct} when is_struct(struct) ->
-          convert(struct, false)
-
-        {tag, attrs, list} when is_list(list) ->
-          value = Enum.map(list, fn struct -> convert(struct, false) end)
-          {tag, attrs, value}
-
-        {tag, attrs, value} ->
-          XmlBuilder.element(tag, attrs, value)
-      end)
+      |> Enum.map(&get_value_for_tag(module, &1))
+      |> Enum.reject(&tag_empty?/1)
+      |> Enum.map(&create_xml_element_from_content/1)
 
     if top_level do
       doc = XmlBuilder.document(tag, attributes, content)
@@ -40,5 +21,28 @@ defmodule ExSzamlazzHu.Utils.StructToXML do
     else
       XmlBuilder.element(tag, attributes, content)
     end
+  end
+
+  defp get_value_for_tag(module, tag) when is_atom(tag), do: {tag, nil, Map.get(module, tag)}
+  defp get_value_for_tag(_, other), do: other
+
+  defp tag_empty?({_, _, value}) when is_nil(value), do: true
+  defp tag_empty?(_), do: false
+
+  defp create_xml_element_from_content(struct) when is_struct(struct) do
+    convert(struct, false)
+  end
+
+  defp create_xml_element_from_content({_, _, struct}) when is_struct(struct) do
+    convert(struct, false)
+  end
+
+  defp create_xml_element_from_content({tag, attrs, list}) when is_list(list) do
+    value = Enum.map(list, fn struct -> create_xml_element_from_content(struct) end)
+    XmlBuilder.element(tag, attrs, value)
+  end
+
+  defp create_xml_element_from_content({tag, attrs, value}) do
+    XmlBuilder.element(tag, attrs, value)
   end
 end
